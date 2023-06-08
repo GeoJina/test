39f07304edefab17c38a3b43d0bcf1803600c86e
GeoJina <131639063+GeoJina@users.noreply.github.com>
2023-06-08 17:45:36 +0900

    first commit

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", pEvent => {
+    console.log("서비스 워커 설치 완료");
+})
+
+self.addEventListener("activate", pEvent => {
+    console.log("서비스 워커 동작 실행");
+})
+
+self.addEventListener("fetch", pEvent => {
+    console.log("데이터 요청(fetch)", pEvent);
+})
